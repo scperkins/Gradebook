4abae13295f48f1553633b9ab098943abc3e767b
Sean Perkins <perkinsean@gmail.com>
2015-03-20 14:01:22 -0400

    added page that queries for students in db and displays themin a table

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, g, request, render_template
 from models import *
-#import views
 
 app = Flask(__name__)
 
@@ -20,11 +19,25 @@ def after_request(response):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    #if request.method == 'POST'
     return render_template('index.html')
 
+@app.route('/students/add', methods=['GET', 'POST'])
+def add_students():
+    if request.method == 'POST':
+        with database.transaction():
+            student = Student.create(
+                    first_name=request.form['first_name'],
+                    middle_initial=request.form['middle_initial'],
+                    last_name=request.form['last_name'],
+                    grad_year=request.form['grad_year'],
+                    gpa=request.form['gpa'])
+    
+    return render_template('add.html')
+
+@app.route('/students/')
 def get_students():
     students= Student.select()
+    return render_template('students.html',students=students)
 
 if __name__ == "__main__":
     app.run(debug=True)
