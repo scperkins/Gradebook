7d90f6a99efd460dc7a6425ef08b9d9522702d8d
Sean Perkins <perkinsean@gmail.com>
2015-03-20 16:35:07 -0400

    working student add form, links, etc

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, g, request, render_template
+from flask import Flask, g, request, render_template, flash, redirect, url_for
 from models import *
 
+SECRET_KEY = 'development key'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 def create_tables():
     database.connect()
@@ -24,15 +27,21 @@ def index():
 @app.route('/students/add', methods=['GET', 'POST'])
 def add_students():
     if request.method == 'POST':
-        with database.transaction():
-            student = Student.create(
-                    first_name=request.form['first_name'],
-                    middle_initial=request.form['middle_initial'],
-                    last_name=request.form['last_name'],
-                    grad_year=request.form['grad_year'],
-                    gpa=request.form['gpa'])
+        try:
+            with database.transaction():
+                student = Student.create(
+                        first_name=request.form['first_name'],
+                        middle_initial=request.form['middle_initial'],
+                        last_name=request.form['last_name'],
+                        grad_year=request.form['grad_year'],
+                        gpa=request.form['gpa'])
+            flash('Student sucessfully added!')
+            return redirect(url_for('get_students'))
+        
+        except IntegrityError:
+            flash('Something went wrong...')
     
-    return render_template('add.html')
+    return render_template('add_students.html')
 
 @app.route('/students/')
 def get_students():
