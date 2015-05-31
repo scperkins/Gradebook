065b08159a8bc4125db48603075839e4076b7884
Sean Perkins <perkinsean@gmail.com>
2015-05-31 17:09:09 -0400

    .

--- forms.py
+++ forms.py
@@ -8,7 +8,9 @@ from flask_wtf import Form
 from wtforms import StringField, DateField, DecimalField, SelectField, TextAreaField, IntegerField, validators
 from wtforms_components import TimeField
 
-GENDER_CHOICE = [('M', 'Male'),('F', 'Female')]
+# No, tumblr feminists, mayonnaise is not a gender.
+GENDER_CHOICE = [('M', 'Male'), ('F', 'Female')]
+# Horseradish isn't a gender either.
 
 class StudentForm(Form):
     first_name = StringField("First Name", [validators.InputRequired()])
@@ -18,6 +20,7 @@ class StudentForm(Form):
     grad_year = DateField("Graduation Year", format='%Y-%m-%d')
     gpa = DecimalField("GPA", [validators.NumberRange(min=0, max=4.0)])
 
+
 class ProfessorForm(Form):
     name = StringField("Name", [validators.InputRequired()])
     gender = SelectField("Gender", choices=GENDER_CHOICE)
@@ -25,11 +28,13 @@ class ProfessorForm(Form):
     hours_start = TimeField("Office Hours Start")
     hours_end = TimeField("Office Hours End")
 
+
 class CourseForm(Form):
     name = StringField("Course Name", [validators.InputRequired()])
     short_course_id = StringField("Course ID")
     credits = DecimalField("Credits")
     professor = SelectField("Professor", coerce=int)
+
 
 class AssignmentForm(Form):
     name = StringField("Assignment Name", [validators.InputRequired()])
